7518e6617bcb46b7cea569c28f81038b266e10eb
lulu_0402 <luizalemessouza14@gmail.com>
2026-02-19 14:02:40 -0300

    Add filter for notas to find aprovados
    Signed-off-by: lulu_0402 <luizalemessouza14@gmail.com>

---
+++ back_end/filter01.js
@@ -0,0 +1,6 @@
+const notas = [4, 8, 5, 16, 9];
+
+// "Filtre as notas onde a nota seja maior ou igual a 7"
+const aprovados = notas.filter(nota => nota >= 9);
+console.log(notas);
+console.log(aprovados);
