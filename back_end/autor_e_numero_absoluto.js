460f3c49bda707570ca2ffb93b44b510fb1ff951
lulu_0402 <luizalemessouza14@gmail.com>
2026-02-25 08:14:08 -0300

    Add HTML program to return absolute value of a number
    Signed-off-by: lulu_0402 <luizalemessouza14@gmail.com>

---
+++ back_end/autor_e_numero_absoluto.js
@@ -0,0 +1,13 @@
+<html>
+    <head>
+    <title> Programa que retorna o valor absoluto de um número </title>
+    </head> Autor: Luiza Lemes
+    
+    
+    <body>
+        <script>
+        var valor1 = Math.abs (-1234);
+        alert (valor1);
+        </script>
+        </body>
+        </html>
