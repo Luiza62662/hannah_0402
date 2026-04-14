8c8ade7bcf35aa6041b4166cf5f51be951bb02e1
lulu_0402 <luizalemessouza14@gmail.com>
2026-04-14 08:14:40 -0300

    Create meu_projeto.js
    Signed-off-by: lulu_0402 <luizalemessouza14@gmail.com>

---
+++ back_end/meu_projeto.js
@@ -0,0 +1,16 @@
+{
+  "name": "minha criação",
+    "version": "1.0.0",
+    "description:" "Meu primeiro projeto",
+    "main": "index.js",
+    "scripts": {
+
+  "start": "webpack --mode production"
+  },
+  "dependencias":{
+    "react": "18.2.0"
+  },
+"deDependencies":{
+  "eslint": "^8,0,0"
+}
+}
